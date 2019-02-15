5ec4f02434bb72f4003f64bd8291a8f1a4bdf2d7
Mike Doig <mike.j.doig@gmail.com>
2019-02-15 21:41:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function increaseRankBy(n) {
  
  function deepestChild() {
    let current = document.getElementById('grand-node');
-   let next = current.children[2];
+   let next = current.children[0];
    while (next) {
      current = next;
      next = current.children[0];
