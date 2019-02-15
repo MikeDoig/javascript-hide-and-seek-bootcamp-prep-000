dffd4010adf87be08ff35a50aac686f091616442
Mike Doig <mike.j.doig@gmail.com>
2019-02-15 21:40:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,10 +18,10 @@ function increaseRankBy(n) {
  
  function deepestChild() {
    let current = document.getElementById('grand-node');
-   let next = current.children[0];
+   let next = current.children[1];
    while (next) {
      current = next;
-     next = current.children[0];
+     next = current.children[1];
    }
    return current;
  }
