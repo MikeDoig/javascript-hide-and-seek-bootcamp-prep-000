a13fa20cd14794da9647dfbe2b0dcc1dec1caf71
Mike Doig <mike.j.doig@gmail.com>
2019-02-15 21:08:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,19 +10,29 @@ function getFirstSelector(selector) {
 //     for (let a=0; a<document.querySelector('.rank').length){
 //   }
 // }
+// function increaseRankBy(n) {
+//   const rank = document.querySelectorAll('.rank');
+
+//   for (let i=0; i < rank.length; i++) {
+//     let children = rank[i].children;
+
+//     for (let x = 0; x < children.length; x++) {
+//       children[x].innerHTML = parseInt(children[x].innerHTML) + n;
+//     }
+//   }
+// }
+ 
 function increaseRankBy(n) {
-  const rank = document.querySelectorAll('.rank');
+  const rankedLists = document.querySelectorAll('.ranked-list');
 
-  for (let i=0; i < rank.length; i++) {
-    let children = rank[i].children;
+  for (let i = 0, l = rankedLists.length; i < l; i++) {
+    let children = rankedLists[i].children;
 
-    for (let x = 0; x < children.length; x++) {
-      children[x].innerHTML = parseInt(children[x].innerHTML) + n;
+    for (let j = 0, k = children.length; j < k; j++) {
+      children[j].innerHTML = parseInt(children[j].innerHTML) + n;
     }
   }
 }
- 
-
  
  
  
