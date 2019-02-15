47660714acc2c7acd21996e0890a96ce2ac3a048
Mike Doig <mike.j.doig@gmail.com>
2019-02-15 21:12:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,29 +10,29 @@ function getFirstSelector(selector) {
 //     for (let a=0; a<document.querySelector('.rank').length){
 //   }
 // }
-// function increaseRankBy(n) {
-//   const rank = document.querySelectorAll('.rank');
-
-//   for (let i=0; i < rank.length; i++) {
-//     let children = rank[i].children;
-
-//     for (let x = 0; x < children.length; x++) {
-//       children[x].innerHTML = parseInt(children[x].innerHTML) + n;
-//     }
-//   }
-// }
- 
 function increaseRankBy(n) {
   const rank = document.querySelectorAll('.ranked-list');
 
-  for (let i = 0, l = rank.length; i < l; i++) {
-    let child = rank[i].children;
+  for (let i=0; i < rank.length; i++) {
+    let children = rank[i].children;
 
-    for (let j = 0, k = child.length; j < k; j++) {
-      child[j].innerHTML = parseInt(child[j].innerHTML) + n;
+    for (let x = 0; x < children.length; x++) {
+      children[x].innerHTML = parseInt(children[x].innerHTML) + n;
     }
   }
 }
+ 
+// function increaseRankBy(n) {
+//   const rank = document.querySelectorAll('.ranked-list');
+
+//   for (let i = 0, l = rank.length; i < l; i++) {
+//     let child = rank[i].children;
+
+//     for (let j = 0, k = child.length; j < k; j++) {
+//       child[j].innerHTML = parseInt(child[j].innerHTML) + n;
+//     }
+//   }
+// }
  
  
  
