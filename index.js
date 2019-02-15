35717c44dccd0db4925691b9b0a4d84fcf321f12
Mike Doig <mike.j.doig@gmail.com>
2019-02-15 21:41:42 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+ function nestedTarget() {
+   return document.querySelector('#nested .target');
+ }
+function increaseRankBy(n) {
+  const rank = document.querySelectorAll('.ranked-list');
 
+  for (let i=0; i<rank.length; i++) {
+    let children = rank[i].children;
+
+    for (let x=0; x<children.length; x++) {
+      children[x].innerHTML = parseInt(children[x].innerHTML) + n;
+    }
+  }
+}
+ 
+ function deepestChild() {
+   let current = document.getElementById('grand-node');
+   let next = current.children[0];
+   while (next) {
+     current = next;
+     next = current.children[0];
+   }
+   return current;
+ }
