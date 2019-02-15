67dae2e48e8156bf95ac69f83c5115f8f2dd5410
Mike Doig <mike.j.doig@gmail.com>
2019-02-15 21:08:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,10 +23,10 @@ function getFirstSelector(selector) {
 // }
  
 function increaseRankBy(n) {
-  const rankedLists = document.querySelectorAll('.ranked-list');
+  const rank = document.querySelectorAll('.ranked-list');
 
-  for (let i = 0, l = rankedLists.length; i < l; i++) {
-    let children = rankedLists[i].children;
+  for (let i = 0, l = rank.length; i < l; i++) {
+    let children = rank[i].children;
 
     for (let j = 0, k = children.length; j < k; j++) {
       children[j].innerHTML = parseInt(children[j].innerHTML) + n;
